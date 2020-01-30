acabf024f30ca42855c1f0bbd753888b00776752
Nail Sharipov <nailxsharipov@gmail.com>
2020-01-30 18:01:25 +0300

    unfinished bit path solution

--- iShapeClipper/Source/Solver/CutSolver.swift
+++ iShapeClipper/Source/Solver/CutSolver.swift
@@ -17,7 +17,6 @@ public struct SolutionResult {
 
 public extension PlainShape {
 
-    
     func cut(path: [IntPoint], iGeom: IntGeom) -> SolutionResult {
         let hull = self.get(index: 0)
         
@@ -41,7 +40,6 @@ public extension PlainShape {
         let shpaeIndex: Int
     }
     
-    
     // если дыра находится внутри полигона
     private func holeCase(cutPath: [IntPoint], iGeom: IntGeom) -> SolutionResult {
         let n = self.layouts.count
@@ -52,136 +50,11 @@ public extension PlainShape {
             let bitList = PlainShapeList(plainShape: PlainShape(points: cutPath))
             return SolutionResult(isInteract: true, mainList: PlainShapeList(plainShape: mainShape), bitList: bitList)
         }
-
-        // новая дыра
-        var superHole = cutPath
-        superHole.invert()
         
-        // дыры которые не пересекаются с новой дырой
-        var notInteractedHoles = [Int]()
-
-        // островки полигона которые оказались внутри новой дыры
-        var shapeIslands = PlainShape.empty
-
-        // попытаемся объединить другие дыры с новой
-        for i in 1..<n {
-            var nextHole = self.get(index: i)
-            nextHole.invert()
-            
-            let unionSolution = Solver.union(master: superHole, slave: nextHole, iGeom: iGeom)
-            
-            switch unionSolution.nature {
-            case .notOverlap:
-                notInteractedHoles.append(i)
-            case .overlap:
-                let uShape = unionSolution.pathList
-                for j in 0..<uShape.layouts.count {
-                    if uShape.layouts[j].isClockWise {
-                        superHole = uShape.get(index: j)
-                    } else {
-                        var island = uShape.get(index: j)
-                        island.invert()
-                        shapeIslands.add(path: island, isClockWise: false)
-                    }
-                }
-
-            }
-        }
+        let bitPaths = self.holeCaseBitSolution(cutPath: cutPath, iGeom: iGeom)
+        let shapePaths = self.holeCaseShapeSolution(cutPath: cutPath, iGeom: iGeom)
         
-        guard !shapeIslands.layouts.isEmpty else {
-            var mainShape = PlainShape(points: self.get(index: 0))
-            mainShape.add(hole: superHole)
-            for j in 0..<notInteractedHoles.count {
-                let index = notInteractedHoles[j]
-                let hole = self.get(index: index)
-                mainShape.add(hole: hole)
-            }
-            // тут ошибка bitList не учитывает пересечения с дырами
-            let bitList = PlainShapeList(plainShape: PlainShape(points: cutPath))
-            return SolutionResult(isInteract: true, mainList: PlainShapeList(plainShape: mainShape), bitList: bitList)
-        }
-
-        // вычитаем из внутрениних островков дыры, которые не пересеклись
-        
-        var result = PlainShapeList(minimumPointsCapacity: shapeIslands.points.count, minimumLayoutsCapacity: 2 * shapeIslands.layouts.count,
-         minimumSegmentsCapacity: shapeIslands.layouts.count)
-
-        // ведем учет дыр которые не участвуют в образовнии островов
-        var usedHoles = Array<Bool>(repeating: false, count: notInteractedHoles.count)
-        var usedHolesCount: Int = 0
-        
-        var j = 0
-        nextIsland:
-        repeat {
-            // должны быть перечисленны по часовой стрелке
-            var island = shapeIslands.get(index: j)
-            var islandHoles = PlainShape.empty
-            for k in 0..<notInteractedHoles.count {
-                let index = notInteractedHoles[k]
-                let hole = self.get(index: index)
-                
-                let diffSolution = Solver.subtract(master: island, slave: hole, iGeom: iGeom)
-                switch diffSolution.nature {
-                case .empty:
-                    // остров совпал с дыркой
-                    j += 1
-                    usedHoles[k] = true
-                    usedHolesCount += 1
-                    continue nextIsland
-                case .notOverlap:
-                    // нет пересечений
-                    break
-                case .overlap:
-                    usedHoles[k] = true
-                    usedHolesCount += 1
-                    if diffSolution.pathList.layouts.count == 1 {
-                        island = diffSolution.pathList.get(index: 0)
-                    } else {
-                        // остров разбился на несколько частей
-                        shapeIslands.remove(index: j)
-                        for s in 0..<diffSolution.pathList.layouts.count {
-                            let part = diffSolution.pathList.get(index: s)
-                            shapeIslands.add(path: part, isClockWise: true)
-                        }
-                        continue nextIsland
-                    }
-                case .hole:
-                    usedHoles[k] = true
-                    usedHolesCount += 1
-                    islandHoles.add(hole: hole)
-                }
-            }
-            var islandShape = PlainShape(points: island)
-            if islandHoles.layouts.count > 0 {
-                for s in 0..<islandHoles.layouts.count {
-                    let hole = islandHoles.get(index: s)
-                    islandShape.add(path: hole, isClockWise: false)
-                }
-            }
-            
-            result.add(plainShape: islandShape)
-
-            j += 1
-        } while j < shapeIslands.layouts.count
-        
-        
-        var mainShape = PlainShape(points: self.get(index: 0))
-        superHole.invert()
-        mainShape.add(hole: superHole)
-        if usedHolesCount != usedHoles.count {
-            for k in 0..<usedHoles.count where !usedHoles[k] {
-                let index = notInteractedHoles[k]
-                let hole = self.get(index: index)
-                mainShape.add(hole: hole)
-            }
-        }
-        result.add(plainShape: mainShape)
-        
-        
-        //  пока не правильный
-        let bitList = PlainShapeList(plainShape: PlainShape(points: []))
-        
-        return SolutionResult(isInteract: true, mainList: result, bitList: bitList)
+        return SolutionResult(isInteract: true, mainList: shapePaths, bitList: bitPaths)
     }
     
     private func overlapCase(cutHullSolution: CutSolution, iGeom: IntGeom) -> SolutionResult {
@@ -266,4 +139,201 @@ public extension PlainShape {
         
         return SolutionResult(isInteract: true, mainList: result, bitList: bitList)
     }
+    
+    
+    private func holeCaseShapeSolution(cutPath: [IntPoint], iGeom: IntGeom) -> PlainShapeList {
+        let n = self.layouts.count
+        
+        // новая дыра
+        var superHole = cutPath
+        superHole.invert()
+        
+        // дыры которые не пересекаются с новой дырой
+        var notInteractedHoles = [Int]()
+
+        // островки полигона которые оказались внутри новой дыры
+        var shapeIslands = PlainShape.empty
+        
+        // попытаемся объединить другие дыры с новой
+        for i in 1..<n {
+            var nextHole = self.get(index: i)
+            nextHole.invert()
+            
+            let unionSolution = Solver.union(master: superHole, slave: nextHole, iGeom: iGeom)
+            
+            switch unionSolution.nature {
+            case .notOverlap:
+                notInteractedHoles.append(i)
+            case .overlap:
+                let uShape = unionSolution.pathList
+                for j in 0..<uShape.layouts.count {
+                    if uShape.layouts[j].isClockWise {
+                        superHole = uShape.get(index: j)
+                    } else {
+                        var island = uShape.get(index: j)
+                        island.invert()
+                        shapeIslands.add(path: island, isClockWise: false)
+                    }
+                }
+                
+            }
+        }
+        
+        guard !shapeIslands.layouts.isEmpty else {
+            var mainShape = PlainShape(points: self.get(index: 0))
+            mainShape.add(hole: superHole)
+            for j in 0..<notInteractedHoles.count {
+                let index = notInteractedHoles[j]
+                let hole = self.get(index: index)
+                mainShape.add(hole: hole)
+            }
+            return PlainShapeList(plainShape: mainShape)
+        }
+
+        // вычитаем из внутрениних островков дыры, которые не пересеклись
+        
+        var shapePart = PlainShapeList(minimumPointsCapacity: shapeIslands.points.count, minimumLayoutsCapacity: 2 * shapeIslands.layouts.count, minimumSegmentsCapacity: shapeIslands.layouts.count)
+
+        // ведем учет дыр которые не участвуют в образовнии островов
+        var usedHoles = Array<Bool>(repeating: false, count: notInteractedHoles.count)
+        var usedHolesCount: Int = 0
+        
+        var j = 0
+        nextIsland:
+        repeat {
+            // должны быть перечисленны по часовой стрелке
+            var island = shapeIslands.get(index: j)
+            var islandHoles = PlainShape.empty
+            for k in 0..<notInteractedHoles.count {
+                let index = notInteractedHoles[k]
+                let hole = self.get(index: index)
+                
+                let diffSolution = Solver.subtract(master: island, slave: hole, iGeom: iGeom)
+                switch diffSolution.nature {
+                case .empty:
+                    // остров совпал с дыркой
+                    j += 1
+                    usedHoles[k] = true
+                    usedHolesCount += 1
+                    continue nextIsland
+                case .notOverlap:
+                    // нет пересечений
+                    break
+                case .overlap:
+                    usedHoles[k] = true
+                    usedHolesCount += 1
+                    if diffSolution.pathList.layouts.count == 1 {
+                        island = diffSolution.pathList.get(index: 0)
+                    } else {
+                        // остров разбился на несколько частей
+                        shapeIslands.remove(index: j)
+                        for s in 0..<diffSolution.pathList.layouts.count {
+                            let part = diffSolution.pathList.get(index: s)
+                            shapeIslands.add(path: part, isClockWise: true)
+                        }
+                        continue nextIsland
+                    }
+                case .hole:
+                    usedHoles[k] = true
+                    usedHolesCount += 1
+                    islandHoles.add(hole: hole)
+                }
+            }
+            var islandShape = PlainShape(points: island)
+            if islandHoles.layouts.count > 0 {
+                for s in 0..<islandHoles.layouts.count {
+                    let hole = islandHoles.get(index: s)
+                    islandShape.add(path: hole, isClockWise: false)
+                }
+            }
+            
+            shapePart.add(plainShape: islandShape)
+
+            j += 1
+        } while j < shapeIslands.layouts.count
+        
+        
+        var mainShape = PlainShape(points: self.get(index: 0))
+        superHole.invert()
+        mainShape.add(hole: superHole)
+        if usedHolesCount != usedHoles.count {
+            for k in 0..<usedHoles.count where !usedHoles[k] {
+                let index = notInteractedHoles[k]
+                let hole = self.get(index: index)
+                mainShape.add(hole: hole)
+            }
+        }
+        shapePart.add(plainShape: mainShape)
+        
+        return shapePart
+    }
+    
+    private func holeCaseBitSolution(cutPath: [IntPoint], iGeom: IntGeom) -> PlainShapeList {
+        // откусим от новой дыры все имеющиеся дыры полигона
+    
+        let n = self.layouts.count
+        var subPaths = PlainShape(points: cutPath.reversed())
+        
+        var holes = PlainShape.empty
+        
+        for i in 1..<n {
+            let nextHole = self.get(index: i)
+            
+            var j = 0
+            while j < subPaths.layouts.count {
+                let bitPath = subPaths.get(index: j)
+                let solution = Solver.subtract(master: bitPath, slave: nextHole, iGeom: iGeom)
+                switch solution.nature {
+                case .notOverlap:
+                    break
+                case .overlap:
+                    var isFirstSubPath = true
+                    for k in 0..<solution.pathList.layouts.count {
+                        if solution.pathList.layouts[k].isClockWise {
+                            let subPath = solution.pathList.get(index: j)
+                            if isFirstSubPath {
+                                subPaths.remove(index: j)
+                                isFirstSubPath = false
+                            }
+                            subPaths.add(path: subPath, isClockWise: true)
+                        } else {
+                            let holePath = solution.pathList.get(index: j)
+                            holes.add(path: holePath, isClockWise: false)
+                        }
+                    }
+                    break
+                case .empty:
+                    subPaths.remove(index: j)
+                case .hole:
+                    holes.add(path: nextHole, isClockWise: false)
+                }
+                j += 1
+            }
+        }
+        
+        guard subPaths.layouts.count > 0 else {
+            return PlainShapeList.empty
+        }
+        
+        guard holes.layouts.count > 0 else {
+            return PlainShapeList(plainShape: subPaths)
+        }
+
+        var bitPaths = PlainShapeList.empty
+        
+        for i in 0..<subPaths.layouts.count {
+            let subPath = subPaths.get(index: i)
+            var subShape = PlainShape(points: subPath)
+            for j in 0..<holes.layouts.count {
+                let hole = holes.get(index: j)
+                if subPath.isContain(hole: hole, isClockWise: false) {
+                    subShape.add(hole: hole)
+                }
+            }
+            bitPaths.add(plainShape: subShape)
+        }
+        
+        return bitPaths
+    }
+    
 }
