44fe54b054697ef2fc67b99859f5269194e6f8f7
Nail Sharipov <nailxsharipov@gmail.com>
2020-01-31 17:25:53 +0300

    add test

--- iShapeClipper/Source/TestData/CutTestData.swift
+++ iShapeClipper/Source/TestData/CutTestData.swift
@@ -453,6 +453,38 @@ struct CutTestData {
                 Point(x: 25, y: -10),
                 Point(x: 25, y: 10),
             ].int
+        ),
+        // 13
+        TestData(
+            shape: PlainShape(
+                iShape: IntShape(
+                    hull: [
+                        Point(x: -20, y: 20),
+                        Point(x: 20, y: 20),
+                        Point(x: 20, y: -15),
+                        Point(x: -20, y: -15)
+                    ].int,
+                    holes: [
+                        [
+                            Point(x: -5, y: 5),
+                            Point(x: -5, y: 0),
+                            Point(x: 5, y: 0),
+                            Point(x: 5, y: 5)
+                        ].int,
+                        [
+                            Point(x: -15, y: -5),
+                            Point(x: -15, y: -10),
+                            Point(x: 15, y: -10),
+                            Point(x: 15, y: -5)
+                        ].int
+                    ]
+                )
+            ),
+            path: [
+                Point(x: -5.5, y: 2.5),
+                Point(x: 25, y: -15),
+                Point(x: 25, y: 20),
+            ].int
         )
     ]
 }
